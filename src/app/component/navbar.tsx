2a55977deaa4cb3dc547cdca486c1ecc08a4c36e
henryni <17841553035@163.com>
2025-05-16 22:37:22 +0800

    忘了弄了啥了，也许是

--- src/app/component/navbar.tsx
+++ src/app/component/navbar.tsx
@@ -4,13 +4,20 @@ export default function Nav() {
   return (
         <nav className={styles.nav}>
           <div className={styles.list}> 
-            <ReorderIcon />
+            <button className={styles.iconbutton}><ReorderIcon /></button>
           </div>
           <div className={styles.logoContainer}>
             <img src="/logo.png" alt="Logo" className={styles.logoImage} />
           </div>
     
             <input className={styles.search} type="text" />
+            <div className={styles.divhomelist}>
+              <ul className={styles.homelist}>
+                <li className={styles.ali}>Home</li>
+                <li className={styles.ali}>About</li>
+                <li className={styles.alia}>Contact</li>
+              </ul>
+            </div>
 
           
         </nav>
