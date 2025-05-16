53ebcb1528f96f0a4e265f98fb159311b477e0aa
henryni <17841553035@163.com>
2025-05-16 17:07:05 +0800

    忘了弄了啥了，也许是

--- sister/src/app/component/navbar.tsx
+++ sister/src/app/component/navbar.tsx
@@ -1,11 +1,15 @@
-import styles from './ui/nav.module.css';
-
+import styles from '../ui/nav.module.css';
+import ReorderIcon from '@mui/icons-material/Reorder';
 export default function Nav() {
   return (
         <nav className={styles.nav}>
+          <div className={styles.list}> 
+            <ReorderIcon />
+          </div>
           <div className={styles.logoContainer}>
             <img src="/logo.png" alt="Logo" className={styles.logoImage} />
           </div>
+          
         </nav>
     
 
