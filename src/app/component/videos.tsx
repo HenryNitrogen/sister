bdaa978486f0f98fa712e52393e4e8cbce03bec0
henryni <17841553035@163.com>
2025-05-27 11:40:28 +0800

    忘了弄了啥了，也许是

--- src/app/component/videos.tsx
+++ src/app/component/videos.tsx
@@ -1,3 +1,4 @@
+import { Avatar } from '@mui/material';
 import styles from '../ui/videos.module.css';
 export default function Videos() {
     return(
@@ -7,7 +8,7 @@ export default function Videos() {
                 <div className={styles.infobox}>
                     <div className={styles.au}>
                         <div className={styles.channelicon}>
-                        <img src="/" alt="icon"/>
+                        <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
                         </div>
                         <div className={styles.name}>Channel Name</div>
                     </div>
@@ -22,38 +23,8 @@ export default function Videos() {
 
 
             <div className={styles.cards}>
-            <div className={styles.v}></div>
-                <div className={styles.infobox}>
-                    <div className={styles.au}>
-                        <div className={styles.channelicon}>
-                        <img src="/" alt="icon"/>
-                        </div>
-                        <div className={styles.name}>Channel Name</div>
-                    </div>
-                    <div className={styles.description}>
-                        <p>name</p>
-                        <p>This is a sample description of the video. It provides an overview of the content and what viewers can expect.</p>
-                        <p className={styles.date}>date</p>
-                        <p className={styles.views}>10K views</p>
-                    </div>
-                </div>
             </div>
             <div className={styles.cards}>
-            <div className={styles.v}></div>
-                <div className={styles.infobox}>
-                    <div className={styles.au}>
-                        <div className={styles.channelicon}>
-                        <img src="/" alt="icon"/>
-                        </div>
-                        <div className={styles.name}>Channel Name</div>
-                    </div>
-                    <div className={styles.description}>
-                        <p>name</p>
-                        <p>This is a sample description of the video. It provides an overview of the content and what viewers can expect.</p>
-                        <p className={styles.date}>date</p>
-                        <p className={styles.views}>10K views</p>
-                    </div>
-                </div>
             </div>
             
         </main>
