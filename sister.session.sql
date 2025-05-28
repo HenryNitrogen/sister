5d0d3727d2afff3cc4f0a2b190a12bb3a07d604a
henryni <17841553035@163.com>
2025-05-28 22:18:51 +0800

    忘了弄

---
+++ sister.session.sql
@@ -0,0 +1,6 @@
+
+INSERT INTO Video (id,title, description , VideoUrl, thumbnailUrl, createdAt)
+VALUES
+(3,'Sister Session', 'A session with the sisters discussing their experiences and challenges.', 'https://example.com/videos/sister-session.mp4', 'https://example.com/images/sister-session-thumbnail.jpg', NOW());
+
+SELECT *FROM Video
