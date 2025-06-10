2c16e2359400948fca75e69e1bc5d5dcf32e4374
henryni <17841553035@163.com>
2025-06-10 17:28:31 +0800

    change: usesession => getserversession

--- src/app/component/videos.tsx
+++ src/app/component/videos.tsx
@@ -1,9 +1,8 @@
 'use client'
-import { useSession } from 'next-auth/react';
 import { Avatar } from '@mui/material';
 import styles from '../ui/videos.module.css';
 import { useEffect, useState } from 'react';
-
+import getSession from '@/lib/session';
 
 interface Video {
   id: string;
@@ -14,9 +13,8 @@ interface Video {
   createdAt: Date;
 }
 
-export default function VideosPage() {
+export  default async function VideosPage() {
   const [videos, setVideos] = useState<Video[]>([]);
-  const {  status } = useSession();
 
   useEffect(() => {
     // Fetch videos from API route
@@ -24,34 +22,36 @@ export default function VideosPage() {
       .then(res => res.json())
       .then(data => setVideos(data));
   }, []);
-  if (status === 'loading' || status === 'unauthenticated') {
-    return <div>Loading...</div>;}
-    else if (status === 'authenticated') {
-  return (
+  if (await getSession()) {
+    return (
     <main className={styles.main}>
-      {videos.map((video) => (
-        <div key={video.id} className={styles.cards}>
-          <div className={styles.v}></div>
-          <div className={styles.infobox}>
-            <div className={styles.au}>
-              <div className={styles.channelicon}>
-                <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
-              </div>
-              <div className={styles.name}>Channel Name</div>
+    {videos.map((video) => (
+      <div key={video.id} className={styles.cards}>
+        <div className={styles.v}></div>
+        <div className={styles.infobox}>
+          <div className={styles.au}>
+            <div className={styles.channelicon}>
+              <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
             </div>
-            <div className={styles.description}>
-              <p>{video.title}</p>
-              <p>{video.description}</p>
-              <p className={styles.date}>
-                {new Date(video.createdAt).toLocaleDateString()}
-              </p>
-              <p className={styles.views}>10K views</p>
-            </div>
+            <div className={styles.name}>Channel Name</div>
+          </div>
+          <div className={styles.description}>
+            <p>{video.title}</p>
+            <p>{video.description}</p>
+            <p className={styles.date}>
+              {new Date(video.createdAt).toLocaleDateString()}
+            </p>
+            <p className={styles.views}>10K views</p>
           </div>
         </div>
-      ))}
-    </main>
-    
+      </div>
+    ))}
+  </main>
+    )
+  }
+    else  {
+  return (
+    <div>please login first</div>
   );
 }
 }
---
+++ src/lib/session.ts
@@ -0,0 +1,11 @@
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/lib/auth";
+import type { Session } from "next-auth";
+export default async function getSession(): Promise<boolean> {
+    const session: Session | null = await getServerSession(authOptions);
+    if(session){
+        return true;
+    }else{
+        return false;
+    }
+}
