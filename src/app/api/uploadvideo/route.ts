2f3260eca08228e44e71d23f98432d7bccdc6f91
henryni <17841553035@163.com>
2025-06-20 09:50:12 +0800

    +验证

--- src/app/api/uploadvideo/route.ts
+++ src/app/api/uploadvideo/route.ts
@@ -4,33 +4,39 @@ import { NextResponse } from "next/server";
 import path from "path";
 import prisma from "@/lib/prisma";
 import { getServerSession } from "next-auth";
+import { isAdmin } from "@/lib/admin";
 export async function POST(request: Request) {
-    const session = await getServerSession();
-    const name = session?.user?.name || "Anonymous";
-    const data = await request.formData();
-    const file = data.get('file')as unknown as File;
-    const title = data.get('title') as string;
-    const description = data.get('description') as string;
-    const bytes = await file.arrayBuffer();
-    const buffer = Buffer.from(bytes);
-    const rand = randomUUID();
-    const filename = `${rand}-${file.name}`;
-    const filepath = path.join(process.cwd(), "public/uploads/videos", filename);
-    await writeFile(filepath, buffer);
-    const fileUrl = `/uploads/videos/${filename}`;
-    try{
-        await prisma.video.create({
-            data: {
-                id: rand,
-                title: title+name,
-                description: description,
-                videoUrl : fileUrl,
-                thumbnailUrl: fileUrl,
-            },
-        });
-    }catch (error) {
-        console.error("Error saving video to database:", error);
-        return NextResponse.json({ error: "Failed to save video" }, { status: 500 });
-    }
-    return NextResponse.json({ url: fileUrl });
+    const admin = await isAdmin();
+    if (admin){const session = await getServerSession();
+        const name = session?.user?.name || "Anonymous";
+        const data = await request.formData();
+        const file = data.get('file')as unknown as File;
+        const title = data.get('title') as string;
+        const description = data.get('description') as string;
+        const bytes = await file.arrayBuffer();
+        const buffer = Buffer.from(bytes);
+        const rand = randomUUID();
+        const filename = `${rand}-${file.name}`;
+        const filepath = path.join(process.cwd(), "public/uploads/videos", filename);
+        await writeFile(filepath, buffer);
+        const fileUrl = `/uploads/videos/${filename}`;
+        try{
+            await prisma.video.create({
+                data: {
+                    id: rand,
+                    title: title+name,
+                    description: description,
+                    videoUrl : fileUrl,
+                    thumbnailUrl: fileUrl,
+                },
+            });
+        }catch (error) {
+            console.error("Error saving video to database:", error);
+            return NextResponse.json({ error: "Failed to save video" }, { status: 500 });
+        }
+        return NextResponse.json({ url: fileUrl });}
+        else{
+        return NextResponse.json({ error: "Access Denied" }, { status: 403 });
+        }
+    
 }
